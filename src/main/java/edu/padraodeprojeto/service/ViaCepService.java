976c6f7be4a494630cbed7ca598daf098de7043d
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-10-17 19:12:41 -0300

    FEAT: Adicionando camada service com tasks

---
+++ src/main/java/edu/padraodeprojeto/service/ViaCepService.java
@@ -0,0 +1,14 @@
+package edu.padraodeprojeto.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.padraodeprojeto.model.Endereco;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+    @RequestMapping("/{cep}/json")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+
+}
